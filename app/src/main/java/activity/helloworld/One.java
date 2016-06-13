ac20e4372d05565c20cd5f355e78211f684296c6
yunyanGH <1106511975@qq.com>
2016-06-13 19:53:22 +0800

    第一测试类！

---
+++ app/src/main/java/activity/helloworld/One.java
@@ -0,0 +1,13 @@
+package activity.helloworld;
+
+/**
+ * Created by Administrator on 2016/6/13.
+ */
+public class One {
+
+    @Override
+    public String toString() {
+
+        return "这是一个测试类！";
+    }
+}
